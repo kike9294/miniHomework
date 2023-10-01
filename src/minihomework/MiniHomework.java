5be2e5990755070c4f8adaaacb5f3aea79a8c5e0
enriq <enriq@kike-laptop>
2023-10-01 12:49:39 +0100

    changing the variables names

---
+++ src/minihomework/MiniHomework.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihomework;
+
+import ioutils.IOUtils;
+
+/**
+ *
+ * @author enriq
+ */
+public class MiniHomework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IOUtils userInput = new IOUtils();
+        
+        String name;
+        int age;
+        
+        System.out.println("Welcome to NetBeans!");
+        
+        name = userInput.getUserText("What is your name?");
+        age = userInput.getUserInt("How old are you?", 0);
+        
+        System.out.println("Your name is: " + name);
+        System.out.println("You are " + age + " years old.");
+    }
+    
+}
